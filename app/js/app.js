4e5b97ba7febacc09ee2c37ee56c8b735e683d32
Nicholas Oram <nickoram@gmail.com>
2016-02-03 14:49:56 -0400

    wire up users route and default redirect

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,18 @@
 (function() {
     angular.module('NoteWrangler', ['ngRoute'])
         .config(function($routeProvider) {
-            $routeProvider.when('/notes', {
-                templateUrl: '/app/templates/pages/notes/index.html'
-            });
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'templates/pages/notes/index.html'
+                })
+                .when('/notes', {
+                    templateUrl: 'templates/pages/notes/index.html'
+                })
+                .when('/users/', {
+                    templateUrl: 'templates/pages/users/index.html'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
         });
 })();
