cd98415a333c3ba222fb347d2afa251daee1d662
Nicholas Oram <nickoram@gmail.com>
2016-02-03 15:10:53 -0400

    wire up notes/new route for editting notes

--- app/js/app.js
+++ app/js/app.js
@@ -8,6 +8,9 @@
                 .when('/notes', {
                     templateUrl: 'templates/pages/notes/index.html'
                 })
+                .when('/notes/new', {
+                    templateUrl: 'templates/pages/notes/edit.html'
+                })
                 .when('/users/', {
                     templateUrl: 'templates/pages/users/index.html'
                 })
