f2fd168afc10f948831627059bed13c3ec6a94ab
Nicholas Oram <nickoram@gmail.com>
2016-02-10 15:14:09 -0400

    add isolate scope to nwCard directive
    - allows <nw-card></nw-card> to behave like a function taking params
     - which makes the directive more flexible
    - scope
     - "@" passes in string
     - "=" two-way data bind, no longer need {{}}
     - "&" allows the directive's isolate scope to pass values into
      the parent scope for evaluation in the expression defined in the attribute.

--- app/js/directives/nw-card.js
+++ app/js/directives/nw-card.js
@@ -3,7 +3,11 @@
         .directive("nwCard", function() {
             return {
                 restrict: "E",
-                templateUrl: "templates/directives/nw-card.html"
+                templateUrl: "templates/directives/nw-card.html",
+                scope: {
+                    header: "@",
+                    description: "@"
+                }
             };
         });
 })();
