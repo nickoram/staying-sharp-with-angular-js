b38bad49785d5dddcb5fcde1d69c8b56ccd6877b
Nicholas Oram <nickoram@gmail.com>
2016-02-11 16:45:49 -0400

    introducing services with a NoteFactory
    - doesn't do anything yet

---
+++ app/js/services/note.js
@@ -0,0 +1,13 @@
+(function() {
+    angular.module('NoteWrangler')
+        .factory('Note', function NoteFactory() {
+            return {
+                all: function() {
+                    // return something cool
+                },
+                create: function() {
+                    // return something cool
+                }
+            };
+        });
+})();
